fc4af3235df66e62b77be254c521a9c7814cb9f8
Johannes <johannes.haapanen@gmail.com>
2022-03-24 13:38:43 +0200

    New build system

--- Makefile
+++
@@ -1,9 +0,0 @@
-pzip: main.o
-	gcc main.o -Wall -std=c99 -o pzip
-
-main.o: main.c
-	gcc -Wall -std=c99 -c main.c
-
-clean:
-	rm *.o pzip
-
--- files.c
+++ files.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include "files.h"
 
-int main(){
-
-  int res = test_fn();
-
+int test_fn(void){
+  
+  printf("Hello World \n");
   return 0;
 }
